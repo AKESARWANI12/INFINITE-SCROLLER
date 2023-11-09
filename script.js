8d9020180c469af7fcf68d49be95d2c082a2ba97
AKESARWANI12 <95116904+AKESARWANI12@users.noreply.github.com>
2023-11-09 14:16:08 +0530

    added more functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+// how to add scrolling effect
+const container=document.querySelector('.container');
+let limit=4;
+let pageCount=1;
+let postCount=1;
+const getPost=async ()=>{
+    const response=await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${pageCount}`)
+    const data=await response.json();
+    // console.log(data.body);
+
+
+    data.map((curElm,index)=>{
+         const htmlData=`
+         <div class="posts">
+         <p class="post-id">${postCount++}</p>  
+         <h2 class="title">${curElm.title}</h2>
+          <p class="post-info">${curElm.body}</p>
+     </div>`;
+     
+     container.insertAdjacentHTML('beforeend',htmlData);
+    });
+ 
+}
+getPost();
+const showData=()=>{
+    setTimeout(()=>{
+    //  pageCount++;
+     getPost();
+    },300)
+}
+window.addEventListener('scroll',()=>{
+    const {scrollHeight,scrollTop,clientHeight}=document.documentElement;
+
+    if(scrollTop + clientHeight>=scrollHeight){
+        console.log('i am at bottom');
+       showData();
+    }
+})
